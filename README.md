539372d64bbcfca78c558c6a72a839a28f749b80
monet2155 <monet9812@gmail.com>
2025-05-29 16:38:40 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fortune_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/fortune_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fortune_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
